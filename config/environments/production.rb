b2f493116181ef7a1e0e06536312a2a81aad112f
AkitaOnRails <fabioakita@gmail.com>
2014-07-28 14:48:07 -0300

    Adding send grid configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,5 +63,15 @@ if defined?(FatFreeCRM::Application)
     config.active_support.deprecation = :notify
 
     config.action_mailer.default_url_options = { :host => 'crm.codeminer42.com' }
+
+    ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+    }
   end
 end
